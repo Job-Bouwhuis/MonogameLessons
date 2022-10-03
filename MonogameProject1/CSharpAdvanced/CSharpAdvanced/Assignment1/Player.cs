47aed444ef32fc9d2435fe90baa7c0528792875e
Job <0326531@student.rocvantwente.nl>
2022-10-03 11:31:02 +0200

    first commit.

---
+++ MonogameProject1/CSharpAdvanced/CSharpAdvanced/Assignment1/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SnowLibrary.Monogame;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace CSharpAdvanced.Assignment1
+{
+    internal class Player : GameObject
+    {
+        private int walkSpeed;
+        public Player(int walkSpeed) : base("new Object", new Transform())
+        {
+            this.walkSpeed = walkSpeed;   
+        }
+
+        public override void Update()
+        {
+            Transform lastPos = transform;
+            if (Input.GetKeyDown(Keys.W))
+                transform.position.Y -= walkSpeed;
+            if (Input.GetKeyDown(Keys.A))
+                transform.position.X -= walkSpeed;
+            if (Input.GetKeyDown(Keys.D))
+                transform.position.X += walkSpeed;
+            if (Input.GetKeyDown(Keys.S))
+                transform.position.Y += walkSpeed;
+
+           
+            if (!MonoUtils.AreYouOnScreen(transform.position, texture))
+            {
+                transform = lastPos;
+            }
+        }
+    }
+}
---
+++ MonogameProject1/CSharpAdvanced/CSharpAdvanced/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpAdvanced
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using var game = new Assignment1.Game1();
+            game.Run();
+        }
+    }
+}
